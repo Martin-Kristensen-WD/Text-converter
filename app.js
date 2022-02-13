96c67a815c7e42d2011b219e46df9a8e22c028d8
Martin Kristensen <matinndkk@hotmail.com>
2022-02-13 15:17:40 +0100

    added repeat word logic

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const spacesInput = document.querySelector('#noSpaces');
 const spacesResult = document.querySelector('#spaces-result');
 const spacesSubmit = document.querySelector('#to-nospaces');
 
+// Repeat word
+const repeatInput = document.querySelector('#repeat');
+const selected = document.querySelector('#selected');
+const repeatResult = document.querySelector('#repeat-result');
+const repeatSubmit = document.querySelector('#repeat-sentence');
+
 
 // FUNCTIONS
 // Convert input value and return as uppercase
@@ -34,6 +40,17 @@ function removeSpace() {
    return spacesResult.innerHTML = noSpace;
 }
 
+// Repeat word
+function repeat() {
+   let sentence = repeatInput.value;
+   let numberOfTimes = selected.value;
+   const wordArray = [];
+   for (let i = 1; i <= numberOfTimes; i++) {
+      wordArray.push(sentence);
+   }
+   return repeatResult.innerHTML = wordArray.join(' ');
+}
+
 
 // Convert to uppercase
 uppercaseSubmit.addEventListener('click', toUppercase);
@@ -43,3 +60,6 @@ reverseSubmit.addEventListener('click', reverseText);
 
 // Remove spaces
 spacesSubmit.addEventListener('click', removeSpace);
+
+// Repeat sentence
+repeatSubmit.addEventListener('click', repeat);
